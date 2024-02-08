927dbb4152e82719be64f1372d180958caff7476
eyorokumo <eyorokumodicksonemmanuel@gmail.com>
2024-02-09 00:35:02 +0100

    jonas class

---
+++ Javacript/script.js
@@ -0,0 +1,117 @@
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN!");
+
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// // Values and varible
+
+// console.log("Smart");
+// console.log("28");
+
+// let firstName = "Smartd";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// // variable naming conventions
+// let Smart_Dickson = "SD";
+// let $function = 27;
+
+// let person = "smart";
+// let PI = 3.1415;
+
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+// // Data Types (a) Object (b) Primitive
+// //The 7 Primitive Data Types
+// true;
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // typeof
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // // console.log(typeof 29);
+// // // console.log(typeof "Smart");
+
+// // javascriptIsFun = "YES!";
+// // console.log(typeof javascriptIsFun);
+
+// // //
+// // let year;
+// // console.log(year);
+// // console.log(typeof year);
+
+// // year = 1991;
+// // console.log(typeof year);
+
+// // console.log(typeof null);
+
+// LET ,CONST AND VAR
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// // const job;
+
+// var job = "programmer";
+// job = "teacher";
+
+// lastName = "Jobman";
+// console.log(lastName);
+
+//BASIC OPERATORS
+// maths operators
+// const now = 2037;
+// const ageJobman = now - 1991;
+// const agesarah = now - 2018;
+// console.log(ageJobman, agesarah);
+
+// console.log(ageJobman * 2, ageJobman / 10, 2 ** 3);
+// //2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Jobman";
+// const lastName = "dallon";
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+
+// let x = 10 + 5; //15
+// x += 10; //x = x + 10 = 25
+// x *= 4; //x = x * 4 = 100
+// x++; // x = x + 1
+// x--;
+// x--;
+// console.log(x);
+
+// // Comparsion operators
+// console.log(ageJobman > agesarah); // >,<,>=,<=
+
+// console.log(agesarah >= 18);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+// Operator Precedence
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10, x =10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
